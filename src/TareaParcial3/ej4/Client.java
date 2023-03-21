28b7149911712c50d5f5363aaae0cf0d7f5f8c3f
LucianoVargas <lutyvr02@gmail.com>
2023-03-21 19:26:17 -0400

    final practice

--- src/TareaParcial3/ej4/Client.java
+++ src/TareaParcial3/ej4/Client.java
@@ -1,12 +1,13 @@
 package TareaParcial3.ej4;
 
 public class Client {
+    //contraseña de 8 caracteres de palabras y letras
     public static void main (String []args){
         ServerProxy fileServer = new ServerProxy("10.20.10.10","8080","500TB");
-        fileServer.uploadUsuario(new Usuario("lutyvr02","asdasds"));
-        fileServer.uploadUsuario(new Usuario("Bussy888","asdasdasd"));
-        fileServer.uploadUsuario(new Usuario("Trevor","asdsadas"));
-        fileServer.uploadUsuario(new Usuario("EnzoFernandez","asdsadas"));
-        fileServer.showAllFile();
+        fileServer.uploadUsuario(new Usuario("lutyvr02","Stream12"));
+        fileServer.uploadUsuario(new Usuario("Bussy888","Stream12"));
+        fileServer.uploadUsuario(new Usuario("Trevor","Stream12"));
+        fileServer.uploadUsuario(new Usuario("EnzoFernandez","Stream12"));
+        fileServer.MostrarUsuario();
     }
 }
